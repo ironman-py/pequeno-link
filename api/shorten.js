4c504b43dc58e8b98bd95b64f13217f62c93a3dc
ironman-py <limaleandrodev@gmail.com>
2025-01-18 21:13:33 -0300

    mudanças

--- api/shorten.js
+++ api/shorten.js
@@ -3,9 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('MongoDB connected'))
-    .catch(err => console.error('MongoDB connection error:', err));
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const linkSchema = new mongoose.Schema({
     originalUrl: String,
